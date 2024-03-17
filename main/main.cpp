6d16cc5abd7169a086fe055d9548372208bf4575
Michael Gormack <michaelg@adherium.com>
2024-03-17 23:31:41 +1300

    try to get it building

--- main/main.cpp
+++ main/main.cpp
@@ -5,15 +5,20 @@
  */
 
 #include <stdio.h>
-
+#include <string.h>
 #include <iostream>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_event.h>
+#include <esp_log.h>
 
 #include <hap.h>
 #include <hap_apple_servs.h>
 #include <hap_apple_chars.h>
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include <app_wifi.h>
+#include <app_hap_setup_payload.h>
+
 #include "esp_timer.h"
 #include "esp_lcd_panel_io.h"
 #include "esp_lcd_panel_vendor.h"
@@ -105,8 +110,14 @@ esp_lcd_touch_handle_t tp = NULL;
 #define VALVE_TASK_STACKSIZE 4 * 1024
 #define VALVE_TASK_NAME      "hap_valve"
 
+#define CONFIG_EXAMPLE_SETUP_CODE   "111-22-333"
+#define CONFIG_EXAMPLE_SETUP_ID     "ES32"
 
 extern void example_lvgl_demo_ui(lv_disp_t *disp);
+static int valve_identify(hap_acc_t *ha);
+static int valve_read(hap_char_t *hc, hap_status_t *status_code, void *serv_priv, void *read_priv);
+static int valve_write(hap_write_data_t write_data[], int count, void *serv_priv, void *write_priv);
+static void valve_hap_event_handler(void* arg, esp_event_base_t event_base, int32_t event, void *data);
 
 static bool example_notify_lvgl_flush_ready(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx)
 {
@@ -254,8 +265,168 @@ static void valve_thread_entry(void *p)
 
     /* Initialize the HAP core */
     hap_init(HAP_TRANSPORT_WIFI);
+
+    /* Initialise the mandatory parameters for Accessory which will be added as
+     * the mandatory services internally
+     */
+    hap_acc_cfg_t cfg = {
+        .name = (char*)"Esp-Valve",
+        .model = (char*)"EspValve01",
+        .manufacturer = (char*)"MG",
+        .serial_num = (char*)"001122334455",
+        .fw_rev = (char*)"1.0.0",
+        .hw_rev = NULL,
+        .pv = (char*)"1.1.0",
+        .cid = HAP_CID_SPRINKLER,
+        .identify_routine = valve_identify,
+    };
+
+     /* Create accessory object */
+    accessory = hap_acc_create(&cfg);
+
+    /* Add a dummy Product Data */
+    uint8_t product_data[] = {'E','S','P','3','2','H','A','P'};
+    hap_acc_add_product_data(accessory, product_data, sizeof(product_data));
+
+    /* Add Wi-Fi Transport service required for HAP Spec R16 */
+    hap_acc_add_wifi_transport_service(accessory, 0);
+
+    /* Create the Fan Service. Include the "name" since this is a user visible service  */
+    service = hap_serv_irrigation_system_create(0,0,0);
+    hap_serv_add_char(service, hap_char_name_create((char*)"My Valve"));
+    //hap_serv_add_char(service, hap_char_rotation_direction_create(0));
+
+    /* Set the write callback for the service */
+    hap_serv_set_write_cb(service, valve_write);
+
+    /* Set the read callback for the service (optional) */
+    hap_serv_set_read_cb(service, valve_read);
+
+    /* Add the Fan Service to the Accessory Object */
+    hap_acc_add_serv(accessory, service);
+
+    /* Add the Accessory to the HomeKit Database */
+    hap_add_accessory(accessory);
+
+    /* Register a common button for reset Wi-Fi network and reset to factory.
+     */
+   // reset_key_init(RESET_GPIO);
+
+   ESP_LOGI(TAG, "Accessory is paired with %d controllers",
+                hap_get_paired_controller_count());
+
+     /* Unique Setup code of the format xxx-xx-xxx. Default: 111-22-333 */
+    hap_set_setup_code(CONFIG_EXAMPLE_SETUP_CODE);
+    /* Unique four character Setup Id. Default: ES32 */
+    hap_set_setup_id(CONFIG_EXAMPLE_SETUP_ID);
+
+    #ifdef CONFIG_APP_WIFI_USE_WAC_PROVISIONING
+    app_hap_setup_payload((char*)CONFIG_EXAMPLE_SETUP_CODE, (char*)CONFIG_EXAMPLE_SETUP_ID, true, cfg.cid);
+#else
+    app_hap_setup_payload((char*)CONFIG_EXAMPLE_SETUP_CODE, (char*)CONFIG_EXAMPLE_SETUP_ID, false, cfg.cid);
+#endif
+
+	 /* Initialize Wi-Fi */
+    app_wifi_init();
+
+    /* Register an event handler for HomeKit specific events.
+     * All event handlers should be registered only after app_wifi_init()
+     */
+    esp_event_handler_register(HAP_EVENT, ESP_EVENT_ANY_ID, &valve_hap_event_handler, NULL);
+
+    /* After all the initializations are done, start the HAP core */
+    hap_start();
+    /* Start Wi-Fi */
+    app_wifi_start(portMAX_DELAY);
+    /* The task ends here. The read/write callbacks will be invoked by the HAP Framework */
+    vTaskDelete(NULL);
 }
 
+/**
+ * The Reset button  GPIO initialisation function.
+ * Same button will be used for resetting Wi-Fi network as well as for reset to factory based on
+ * the time for which the button is pressed.
+ */
+/*static void reset_key_init(uint32_t key_gpio_pin)
+{
+    button_handle_t handle = iot_button_create(key_gpio_pin, BUTTON_ACTIVE_LOW);
+    iot_button_add_on_release_cb(handle, RESET_NETWORK_BUTTON_TIMEOUT, reset_network_handler, NULL);
+    iot_button_add_on_press_cb(handle, RESET_TO_FACTORY_BUTTON_TIMEOUT, reset_to_factory_handler, NULL);
+}*/
+
+
+static int valve_identify(hap_acc_t *ha)
+{
+    ESP_LOGI(TAG, "Accessory identified");
+    return HAP_SUCCESS;
+}
+
+static int valve_read(hap_char_t *hc, hap_status_t *status_code, void *serv_priv, void *read_priv)
+{
+   /* if (hap_req_get_ctrl_id(read_priv)) {
+        ESP_LOGI(TAG, "Received read from %s", hap_req_get_ctrl_id(read_priv));
+    }
+    if (!strcmp(hap_char_get_type_uuid(hc), HAP_CHAR_UUID_ROTATION_DIRECTION)) {
+
+        const hap_val_t *cur_val = hap_char_get_val(hc);
+
+        hap_val_t new_val;
+        if (cur_val->i == 1) {
+            new_val.i = 0;
+        } else {
+            new_val.i = 1;
+        }
+        hap_char_update_val(hc, &new_val);
+        *status_code = HAP_STATUS_SUCCESS;
+    }*/
+    return HAP_SUCCESS;
+}
+
+/* A dummy callback for handling a write on the "On" characteristic of Fan.
+ * In an actual accessory, this should control the hardware
+ */
+static int valve_write(hap_write_data_t write_data[], int count, void *serv_priv, void *write_priv)
+{
+	return HAP_SUCCESS;
+}
+
+static void valve_hap_event_handler(void* arg, esp_event_base_t event_base, int32_t event, void *data)
+{
+	switch(event) {
+        case HAP_EVENT_PAIRING_STARTED :
+            ESP_LOGI(TAG, "Pairing Started");
+            break;
+        case HAP_EVENT_PAIRING_ABORTED :
+            ESP_LOGI(TAG, "Pairing Aborted");
+            break;
+        case HAP_EVENT_CTRL_PAIRED :
+            ESP_LOGI(TAG, "Controller %s Paired. Controller count: %d",
+                        (char *)data, hap_get_paired_controller_count());
+            break;
+        case HAP_EVENT_CTRL_UNPAIRED :
+            ESP_LOGI(TAG, "Controller %s Removed. Controller count: %d",
+                        (char *)data, hap_get_paired_controller_count());
+            break;
+        case HAP_EVENT_CTRL_CONNECTED :
+            ESP_LOGI(TAG, "Controller %s Connected", (char *)data);
+            break;
+        case HAP_EVENT_CTRL_DISCONNECTED :
+            ESP_LOGI(TAG, "Controller %s Disconnected", (char *)data);
+            break;
+        case HAP_EVENT_ACC_REBOOTING :
+            {
+	            char *reason = (char *)data;
+	            ESP_LOGI(TAG, "Accessory Rebooting (Reason: %s)",  reason ? reason : "null");
+	            break;
+			}
+        case HAP_EVENT_PAIRING_MODE_TIMED_OUT :
+            ESP_LOGI(TAG, "Pairing Mode timed out. Please reboot the device.");
+            break;
+        default:
+            /* Silently ignore unknown events */
+            break;
+    }
+}
 
 extern "C" void app_main(void)
 {
